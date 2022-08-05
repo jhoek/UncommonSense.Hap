14abe4ab2537410bfc5d9a1a6fae9ed7e7fb6c10
Jan Hoek <jhoek@uncommonsense.nl>
2022-08-05 11:57:30 +0200

    More work on test

--- test.ps1
+++ test.ps1
@@ -9,6 +9,16 @@
 # | Select-HtmlNode -CssSelector '.teaser--compact h3' -All
 # | Select-Object -ExpandProperty InnerText
 
+$Property = @{
+    DateTime    = 'h5.modal-title'
+    Title       = 'h2.text-color-secondary'
+    Speakers    = 'h2.accordion-header button'
+    Tags        = 'span.tag'
+    Description = '.pt-3 p'
+}
+
 ConvertTo-HtmlDocument -Uri https://bctechdays.com/event
 | Select-HtmlNode -CssSelector '.modal[aria-labelledby^="event-detail-"] .modal-content' -All
-| Convert-HtmlNode @{DateTime = 'h5.modal-title'; Title = 'h2.text-color-secondary'; Speakers = 'h2.accordion-header button' } -TypeName 'UncommonSense.BcTechDays.ScheduledEvent' -Mode CssSelector -Verbose
+| Convert-HtmlNode -Property $Property -TypeName 'UncommonSense.BcTechDays.ScheduledEvent' -Mode CssSelector
+
+# FIXME: Description
