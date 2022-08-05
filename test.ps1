940538a3143655ad2ae1a57e3229a0925b7c25f7
Jan Hoek <jhoek@uncommonsense.nl>
2022-08-05 12:05:12 +0200

    Moved code to UncommonSense.BcTechDays

--- test.ps1
+++ test.ps1
@@ -8,17 +8,3 @@
 # | ConvertTo-HtmlDocument
 # | Select-HtmlNode -CssSelector '.teaser--compact h3' -All
 # | Select-Object -ExpandProperty InnerText
-
-$Property = [Ordered]@{
-    DateTime    = 'h5.modal-title'
-    Title       = 'h2.text-color-secondary'
-    Speakers    = 'h2.accordion-header button'
-    Tags        = 'span.tag'
-    Description = '.pt-3 p'
-}
-
-ConvertTo-HtmlDocument -Uri https://bctechdays.com/event
-| Select-HtmlNode -CssSelector '.modal[aria-labelledby^="event-detail-"] .modal-content' -All
-| Convert-HtmlNode -Property $Property -TypeName 'UncommonSense.BcTechDays.ScheduledEvent' -Mode CssSelector
-
-# FIXME: Description
