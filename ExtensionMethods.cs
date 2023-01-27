de00e3f6c16366bec82165939df7cbf2c0175906
Jan Hoek <jhoek@uncommonsense.nl>
2023-01-27 19:51:45 +0100

    Introduced Get-HtmlNodeText

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -17,4 +17,26 @@ public static class ExtensionMethods
 
     public static string RegexReplace(this string input, string pattern, string replacement) =>
         Regex.Replace(input, pattern, replacement);
+
+    public static string JoinString<T>(this IEnumerable<T> items, string separator) =>
+        string.Join(separator, items);
+
+    public static string GetHtmlNodeText(
+        this IEnumerable<HtmlNode> nodes,
+        bool directInnerTextOnly = false,
+        bool skipDeentitize = false,
+        bool skipTrim = false,
+        bool skipRemoveLineBreaks = false,
+        bool SkipFlattenWhitespace = false,
+        bool allowEmptyStrings = false,
+        string separator = " "
+    ) =>
+        nodes
+            .Select(n => directInnerTextOnly ? n.GetDirectInnerText() : n.InnerText)
+            .Select(t => skipDeentitize ? t : HttpUtility.HtmlDecode(t))
+            .Select(t => skipTrim ? t : t.Trim())
+            .Select(t => skipRemoveLineBreaks ? t : t.RegexReplace(@"\n", ""))
+            .Select(t => SkipFlattenWhitespace ? t : t.RegexReplace(@"\s{2,}", " "))
+            .Where(t => allowEmptyStrings || !string.IsNullOrEmpty(t))
+            .JoinString(separator);
 }
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,4 @@ global using HtmlAgilityPack;
 global using HtmlAgilityPack.CssSelectors.NetCore;
 global using System.Collections;
 global using System.Management.Automation;
+global using System.Web;
